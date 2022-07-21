2570f19fd14a4e635d23a5b50db1417c5dd2c1a2
sogoyayo <sogo.oyerinde@gmail.com>
2022-07-21 16:30:20 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit placeat, nisi consequuntur, ad illo quia et corporis, consequatur adipisci commodi nostrum aliquam? Ex inventore expedita repellat consequatur, dolores aliquam eveniet numquam beatae quae dolorem ipsum in nulla! Explicabo, ipsum quaerat.
+      </p>
+    </section>
+  )
+}
+
+export default About
